8039fea8826ae623b596274f68f81c6c17d7d833
DucTranNFQ <duc.tran@nfq.asia>
2022-06-02 15:33:26 +0700

    add: basic structure react router

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import {Routes, Route, Link, Outlet, useParams} from "react-router-dom";
 import 'antd/dist/antd.css';
 
 import {LoginPage, SignupPage} from "./pages/authPage";
-import ReactRouterExample from "./pages/reactRouterExample"
+import Settings from "./pages/Settings"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
 
   return (
-      <ReactRouterExample />
-//     <Routes>
-//       <Route path="auth" element={<div className="formBackground"><Outlet /></div>}>
-//           <Route path="signup" element={<SignupPage />} />
-//           <Route path="login" element={<LoginPage />} />
-//       </Route>
-//     </Routes>
+    <Routes>
+      <Route path="auth" element={<div className="formBackground"><Outlet /></div>}>
+          <Route path="signup" element={<SignupPage />} />
+          <Route path="login" element={<LoginPage />} />
+      </Route>
+      <Route path="settings" element={<Settings />} />
+      <Route path="dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
